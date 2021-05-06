e987364c50b89a1bbb61ee845fd72b905585e492
Juan Daniel Maya Pérez <juan.maya402@gmail,com>
2021-05-06 17:57:44 -0500

    Simplificación Código

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var dado1,dado2,boton_tirar;
 var turno = 0;
 var suma;
 var punto;
-var disputa = false;
+
 
 
 
@@ -40,49 +40,40 @@ function jugar(){
 	mostrarMensaje("Turno " + turno + ", buena suerte...");
 	suma = tiro_1+tiro_2; 
 
-	
+	//Primer turno
 	if (turno == 1 ){
 		if (suma == 7 || suma == 11)
 		{
 			mostrarMensaje("Felicidades... sobrevives. Si vuelves a tirar, reiniciarás el juego.");
 			turno=0;
 			punto=0;
-			disputa=false;
 		}
-		if (suma == 2 || suma == 3 || suma == 12 && disputa==false){
+		if (suma == 2 || suma == 3 || suma == 12){
 			mostrarMensaje("Mala suerte, no lograste sobrevivir ni siquiera un turno. El Demonio del Dado toma tu alma y te la arrebata, causándote un dolor punzante que recorre todo tu cuerpo. Vuelve a tirar para volver a intentarlo.");
 			turno=0;
 			punto=0;
-			disputa=false;
 		}
-	}
-	if (turno !== 0 && disputa == false)
-	{
-
-		if(suma == 4 || suma == 5 || suma == 6 || suma == 8 || suma == 9 || suma == 10)
+			if(suma == 4 || suma == 5 || suma == 6 || suma == 8 || suma == 9 || suma == 10)
 		{
 			mostrarMensaje("Has entrado en la zona de disputa, vuelve a sacar " + suma + " antes de sacar 7 y sobrevivirás");
       		punto = suma;
-      		disputa = true;
 			
 		}
-	
-
 	}
 
+
+	//Turnos siguientes
 	if ( turno !== 1 && punto == suma  )
 	{
 		mostrarMensaje("Felicidades... sobrevives. Si vuelves a tirar, reiniciarás el juego.");
 		turno =0;
 		punto = 0;
-		disputa = false;
 	}	
-	if (turno !== 1 && suma == 7 && disputa==true)	
+	if (turno !== 1 && suma == 7)	
 	{
 		mostrarMensaje("Mala Suerte... El Demonio del Dado agarra tu cráneo con una fuerza sobrenatural y lo aplasta lentamente, lo último que experimentas es el sonido de tu propio cerebro destripándose.Vuelve a tirar para volver a intentarlo");
 		turno =0;
 		punto = 0;
-		disputa = false;
 
 	}
 	
